0a7203f1781a5c1a9ff5f00e733088951c0b5bc6
Graziela <grazims26@gmail.com>
2019-06-12 20:13:07 -0700

    fix js each loop

--- js/main.js
+++ js/main.js
@@ -8,8 +8,32 @@ $(".list").on("change", function(event) {
         method: "GET",
         url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${apiKey}`
     }).done(function(data){
-        console.log(data.results);
-        // $(".home").append();
+        $(".image-grid").empty();
+        //  console.log(data.results); 
+         // data.results[].url,
+         // data.results[].title
+         // data.results[].multimedia[4].url
+         let count = 0;
+         data.results.every((element, index) => {
+            console.log(index);
+            //  console.log(element.url);
+            //  console.log(element.title);
+             console.log(element.multimedia);
+             if (index < 12 && element.multimedia.length >= 5 ) {
+                 $(".image-grid").append(`<li><a href="${element.url}"><div class="image-style" style="background-image: url('${element.multimedia[4].url}');"><p>${element.title}</p></div></a></li>`);
+                 //count = count + 1;
+                 return true;           
+             }
+
+             if (index >= 12) {
+                return false;
+             }
+         });
 
     })
   });
+
+//   const array = ["one", "two", "three"]
+//   array.forEach(function (item, index) {
+//     console.log(item, index);
+//   });
